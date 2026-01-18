f1f832d44e86c184e84d5c76dd40a3adda2b54a0
Tran Duy Anh <duyanh30112004@gmail.com>
2026-01-19 00:45:18 +0700

    fix lỗi '===', never use

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import Home from './Components/Home';
-import Login from './Components/Login';
-import Register from './Components/Register';
-import Detail from './Components/Detail';
-import CartPage from './Components/CartPage';
-import Content from './Components/Content';
-import Profile from './Components/Profile';
-import ListProducts from './Components/Admin/ListProducts';
-import EditProducts from './Components/Admin/EditProducts';
-import CreateProducts from './Components/Admin/CreateProducts';
-import Header from './Components/Headers'; 
-import Footer from './Components/Footer';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import Detail from "./Components/Detail";
+import CartPage from "./Components/CartPage";
+import Content from "./Components/Content";
+import Profile from "./Components/Profile";
+import ListProducts from "./Components/Admin/ListProducts";
+import EditProducts from "./Components/Admin/EditProducts";
+import CreateProducts from "./Components/Admin/CreateProducts";
 
 function App() {
   return (
@@ -22,18 +19,45 @@ function App() {
         {/* <Header /> Header component */}
         <Container>
           <Routes>
-            <Route path="/" element={<Content />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/detail/:id" element={<Detail />} />
-            <Route path="/cart" element={<CartPage />} />
+            <Route
+              path="/"
+              element={<Content />}
+            />
+            <Route
+              path="/profile"
+              element={<Profile />}
+            />
+            <Route
+              path="/detail/:id"
+              element={<Detail />}
+            />
+            <Route
+              path="/cart"
+              element={<CartPage />}
+            />
 
             {/* Route that do not include the Header and Footer */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/register"
+              element={<Register />}
+            />
 
-            <Route path="/admin" element={<ListProducts />} />
-            <Route path="/admin/edit/:ProductID" element={<EditProducts />} />
-            <Route path="/admin/create" element={<CreateProducts />} />
+            <Route
+              path="/admin"
+              element={<ListProducts />}
+            />
+            <Route
+              path="/admin/edit/:ProductID"
+              element={<EditProducts />}
+            />
+            <Route
+              path="/admin/create"
+              element={<CreateProducts />}
+            />
           </Routes>
         </Container>
         {/* <Footer /> Footer component */}
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,40 +1,54 @@
-import React from 'react'
-import { Col, Container, Nav, NavItem, NavLink, Row } from 'react-bootstrap'
-import { Cart, Cloud, House, HouseFill, ThreeDotsVertical } from 'react-bootstrap-icons'
+import React from "react";
+import { Col, Container, Nav, Row } from "react-bootstrap";
+import { Cart, Cloud, House } from "react-bootstrap-icons";
 
 export default function Footer() {
   return (
-    <Container fluid className='fixed-bottom bg-light'>
+    <Container
+      fluid
+      className="fixed-bottom bg-light"
+    >
       <Row>
-        <Col sm={6} lg={3}>
-                <Nav.Item className="d-flex flex-column align-items-center">
-                    <House size={24} />
-                    <Nav.Link href="/">Home</Nav.Link>
-                </Nav.Item>
-            <Nav/>
+        <Col
+          sm={6}
+          lg={3}
+        >
+          <Nav.Item className="d-flex flex-column align-items-center">
+            <House size={24} />
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav.Item>
+          <Nav />
         </Col>
-        <Col sm={6} lg={3}>
-                <Nav.Item className="d-flex flex-column align-items-center">
-                    <Cart size={24} />
-                    <Nav.Link href="/cart">Cart</Nav.Link>
-                </Nav.Item>
-            <Nav/>
+        <Col
+          sm={6}
+          lg={3}
+        >
+          <Nav.Item className="d-flex flex-column align-items-center">
+            <Cart size={24} />
+            <Nav.Link href="/cart">Cart</Nav.Link>
+          </Nav.Item>
+          <Nav />
         </Col>
-        <Col sm={6} lg={3}>
-                <Nav.Item className="d-flex flex-column align-items-center">
-                    <Cloud size={24} />
-                    <Nav.Link href="/profile">Profile</Nav.Link>
-                </Nav.Item>
-            <Nav/>
+        <Col
+          sm={6}
+          lg={3}
+        >
+          <Nav.Item className="d-flex flex-column align-items-center">
+            <Cloud size={24} />
+            <Nav.Link href="/profile">Profile</Nav.Link>
+          </Nav.Item>
+          <Nav />
         </Col>
-        <Col sm={6} lg={3}>
-                <Nav.Item className="d-flex flex-column align-items-center">
-                    <Nav.Link href="#home">© 2023 Author</Nav.Link>
-                </Nav.Item>
-            <Nav/>
+        <Col
+          sm={6}
+          lg={3}
+        >
+          <Nav.Item className="d-flex flex-column align-items-center">
+            <Nav.Link href="#home">© 2023 Author</Nav.Link>
+          </Nav.Item>
+          <Nav />
         </Col>
-    </Row>
-        
+      </Row>
     </Container>
-  )
+  );
 }
